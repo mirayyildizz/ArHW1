b091edc7e2bdd70ccca5b01c6c57c891e1d3f31f
Miray Yıldız <miray.yildiz2016@gtu.edu.tr>
2021-11-12 23:30:04 +0300

    AR HW1

---
+++ Assets/vbtn.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class vbtn : MonoBehaviour
+{
+    public GameObject cube;
+    public VirtualButtonBehaviour Vb;	
+    // Start is called before the first frame update
+    void Start()
+    {
+     	Vb.RegisterOnButtonPressed(OnButtonPressed);   
+	Vb.RegisterOnButtonReleased(OnButtonReleased);
+	cube.SetActive(false);
+    }
+
+  
+    public void OnButtonPressed(VirtualButtonBehaviour vb){
+	cube.SetActive(true);
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb){
+	cube.SetActive(false);
+    }	 
+}
